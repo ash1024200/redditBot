a652a415c118efe36af3447f7caed8090b470510
Ashwin Vinod <AshwinV@talentica-all.com>
2017-03-21 16:25:05 +0530

    initialization

---
+++ src/main/java/com/reddit/bot/Application.java
@@ -0,0 +1,34 @@
+package com.reddit.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	@Autowired
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			String quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", String.class);
+			log.info(quote.toString());
+		};
+	}
+}
